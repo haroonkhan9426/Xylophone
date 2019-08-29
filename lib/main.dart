f95db95f12f20e9044a56843a4a45439d26a3182
Haroon khan <haroonkhan9426@gmail.com>
2019-08-29 12:42:31 +0500

    Basic UI done

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,73 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: _renderScreen(),
         ),
       ),
+    );
+  }
+
+  Widget _renderScreen() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 1,
+          child: FlatButton(
+            onPressed: () {},
+            child: Text(' '),
+            color: Colors.red,
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: FlatButton(
+            onPressed: () {}, child: Text(' '),
+            color: Colors.pink,
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: FlatButton(
+            onPressed: () {},
+            child: Text(' '),
+            color: Colors.purple,
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: FlatButton(
+            onPressed: () {},
+            child: Text(' '),
+            color: Colors.blue,
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: FlatButton(
+            onPressed: () {},
+            child: Text(' '),
+            color: Colors.green,
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: FlatButton(
+            onPressed: () {},
+            child: Text(' '),
+            color: Colors.yellow,
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: FlatButton(
+            onPressed: () {},
+            child: Text(' '),
+            color: Colors.brown,
+          ),
+        ),
+
+      ],
     );
   }
 }
